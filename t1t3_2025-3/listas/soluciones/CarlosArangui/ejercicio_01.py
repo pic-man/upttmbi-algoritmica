6fd7cc63508e15ae9e56df3613d9f8c8b8e429e3
Carlos Aranguibel <carlosaranguibel9@gmail.com>
2025-11-08 13:26:15 -0400

    [T1T3][Listas] Agregar ejercicio 1 - Pizzería

---
+++ t1t3_2025-3/listas/soluciones/CarlosArangui/ejercicio_01.py
@@ -0,0 +1,16 @@
+"""
+Ejercicio 1: Pizzería
+Estudiante: Carlos Aranguibel
+GitHub: @CarlosArangui
+Fecha: 2025-10-16
+
+Descripción:
+Programa que gestiona ingredientes de pizza
+"""
+
+# Tu código aquí
+ingredientes = ["queso", "tomate", "anchoas", "jamón"]
+ingredientes.append("pepperoni")
+ingredientes.remove("anchoas")
+print(ingredientes)
+
